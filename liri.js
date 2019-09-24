6b99955187d3b0a99e05a3795237fbbbd3b08be3
Carlo Ortega <cfor0@outlook.com>
2019-09-24 14:53:51 -0400

    no token error on spotify

--- liri.js
+++ liri.js
@@ -11,18 +11,34 @@ if (process.argv[2] === "concert-this") {
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     axios
-    .get(queryUrl)
-    .then(function(response){
-        console.log(response.data[0].venue.name);
-        console.log(response.data[0].venue.city);
-        console.log(moment(response.data[0].datetime).format('L'))
-        
-    })
-    .catch(function(error){
-        if (error.response) {
-            console.log(error.response.data);
+        .get(queryUrl)
+        .then(function (response) {
+            console.log(response.data[0].venue.name);
+            console.log(response.data[0].venue.city);
+            console.log(moment(response.data[0].datetime).format('L'))
+
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+            }
+        })
+} else if (process.argv[2] === "spotify-this-song") {
+    var songName = process.argv[3];
+    var spotify = new Spotify({
+        id: "b00c5a1a3a9241d6a3a98ce9e172d878",
+        secret: "666b69bc26b84fb3aab7ac4af56000d8"
+    });
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
         }
+        console.log(data);
     })
+} else if (process.argv[2] === "movie-this") {
+    var movieName = process.argv[3];
+    axios
+        .get("http://www.omdbapi.com/?t=" + movieName + "apikey=trilogy&")
 }
 
 
